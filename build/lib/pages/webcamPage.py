25230ddd9af206fe73f554d848d0d9936e4453c6
Andrew Hossack <andrew.hossack@aviture.us.com>
2023-07-17 23:29:31 -0600

    DashTools Automatic Push - 07/17/2023 @ 23:29:31

--- build/lib/pages/webcamPage.py
+++
@@ -1,9 +0,0 @@
-import dash_bootstrap_components as dbc
-from dash import Input, Output, dcc, html, dash
-from ..utils import weatherUtils
-
-
-def render() -> html.Div:
-    return html.Div(
-        'Not Done - webcam'
-    )
--- src/app.py
+++ src/app.py
@@ -1,17 +1,3 @@
-'''
-# This app creates a simple sidebar layout using inline style arguments and the
-# dbc.Nav component.
-# dcc.Location is used to track the current location, and a callback uses the
-# current location to render the appropriate page content. The active prop of
-# each NavLink is set automatically according to the current pathname. To use
-# this feature you must install dash-bootstrap-components >= 0.11.0.
-# For more details on building multi-page Dash applications, check out the Dash
-# documentation: https://dash.plot.ly/urls
-# Source: https://dash-bootstrap-components.opensource.faculty.ai/examples/simple-sidebar/
-# @ Create Time: 2023-07-17 20:10:07.921404
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-'''
 import datetime
 
 import dash
@@ -23,15 +9,10 @@ from components import headerComponent, footerComponent
 from pages import homePage, webcamPage, windsAloftPage
 
 app = dash.Dash(
-    title="KTVY Weather App",
+    title="Skydive Utah - Current Weather",
     external_stylesheets=[dbc.themes.BOOTSTRAP],
 )
 
-# Declare server for Heroku deployment. Needed for Procfile.
-server = app.server
-
-# the styles for the main content position it to the right of the sidebar and
-# add some padding.
 app.layout = html.Div(
     [
         dcc.Location(id="url"),
@@ -42,6 +23,8 @@ app.layout = html.Div(
         footerComponent.render()
     ]
 )
+
+server = app.server
 
 ########################
 ###### CALLBACKS #######
