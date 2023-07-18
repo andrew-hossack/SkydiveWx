6d7c954e2ee2ead3a33866faa36576f3512fff26
Andrew Hossack <andrew.hossack@aviture.us.com>
2023-07-18 16:38:32 -0600

    Added time utils

--- src/app.py
+++ src/app.py
@@ -83,10 +83,9 @@ def update_time(n):
 
 
 if __name__ == "__main__":
-    # print('TODO:')
-    # print('\t- Calendar iFrame src')
-    # print('\t- Add historical metar for weather forecasts')
-    # print('\t- Webcam page')
+    print('TODO:')
+    print('\t- Calendar iFrame src')
+    print('\t- Add historical metar for weather forecasts')
     # Improvement: if you pull the forecast at 2:50pm MDT, that's 20:50 UTC, so it used the forecast issued for 20Z. Ideally, you'd want to use 21Z at the point, which you can get by setting hourOffset=1
     # not sure if you want to bother with adding a condition on whether the current time is before or after :30
-    app.run_server(debug=False, port=8050)
+    app.run_server(debug=True, port=8050)
---
+++ src/utils/timeUtils.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+
+from pytz import timezone
+
+
+def zulu_to_mst_string(time):
+    
+    zulu_tz = timezone('UTC')
+    mst_tz = timezone('US/Mountain')
+
+    # If time is a string, convert it to datetime
+    if isinstance(time, str):
+        # Use today's date and the given Zulu hour
+        time = datetime.now().replace(hour=int(time), minute=0, second=0, microsecond=0)
+    
+    # Making the time timezone aware
+    dt = zulu_tz.localize(time)
+
+    # Converting to MST
+    mst_time = dt.astimezone(mst_tz)
+            
+    # Returning the converted time
+    return mst_time.strftime('%-I:%M%p') + " MST"
+
+def convert_utc_to_mst(time):
+    # Make dt timezone aware
+    utc_tz = timezone('UTC')
+    dt = utc_tz.localize(time)
+
+    # Convert to MST
+    mst_tz = timezone('US/Mountain')
+    return dt.astimezone(mst_tz)
+
+
+def time_diff(time):
+    metar_time = convert_utc_to_mst(time)
+    now_time = convert_utc_to_mst(datetime.utcnow())
+    diff = now_time - metar_time
+
+    # Get the difference in days, hours, minutes, seconds
+    seconds = diff.seconds
+    minutes = (seconds % 3600) // 60
+
+    return f'{minutes} minutes ago'
+
+def get_time_now_mst():
+    utc_tz = timezone('UTC')
+    now_utc = utc_tz.localize(datetime.utcnow())
+
+    mst_tz = timezone('US/Mountain')
+    now_mst = now_utc.astimezone(mst_tz)
+
+    return now_mst.strftime('%Y-%m-%d %H:%M:%S %Z%z')
