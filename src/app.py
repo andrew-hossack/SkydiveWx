3a61b3740a8f196492f826afb395e81a68bdd82a
Andrew Hossack <andrew.hossack@aviture.us.com>
2023-07-20 15:07:54 -0600

    remove dev

--- src/app.py
+++ src/app.py
@@ -100,9 +100,9 @@ def refresh_winds(refresh):
 
 
 if __name__ == "__main__":
-    # print('TODO:')
-    # print('\t- Calendar iFrame src')
+    print('TODO:')
+    print('\t- Calendar iFrame src')
     # Improvement: if you pull the forecast at 2:50pm MDT, that's 20:50 UTC, so it used the forecast issued for 20Z. Ideally, you'd want to use 21Z at the point, which you can get by setting hourOffset=1
     # not sure if you want to bother with adding a condition on whether the current time is before or after :30
     # Improvement: Fix the weather direction plot to be continuous
-    app.run_server(debug=False, port=8050)
+    app.run_server(debug=True, port=8050)
